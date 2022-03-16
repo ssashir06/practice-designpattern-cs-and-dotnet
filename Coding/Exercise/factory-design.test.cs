8ddf6358e38d4feca51cd70e3f0f7ac65579b4db
Hiroshi Takeda <takejaponpon+git@gmail.com>
2022-03-17 00:17:36 +0900

    add the first implementation of factory design

---
+++ Coding/Exercise/factory-design.test.cs
@@ -0,0 +1,25 @@
+﻿using Coding.Exercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Coding.Exercise;
+
+public class UnitTestFactory
+{
+    [Fact]
+    public void Test1()
+    {
+        var factory = new PersonFactory();
+        var person1 = factory.CreatePerson("Man A");
+        var person2 = factory.CreatePerson("Woman B");
+
+        Assert.Equal(0, person1.Id);
+        Assert.Equal("Man A", person1.Name);
+        Assert.Equal(1, person2.Id);
+        Assert.Equal("Woman B", person2.Name);
+    }
+}
