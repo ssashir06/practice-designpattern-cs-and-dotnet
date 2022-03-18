1ee13152f73d2cce4b0d5def94f2a64c1d090dfe
Hiroshi Takeda <takejaponpon+git@gmail.com>
2022-03-19 01:55:49 +0900

    add adapter design pattern

---
+++ Coding/Exercise/adapter-design.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Coding.Exercise
+{
+    public class Square
+    {
+        public int Side;
+    }
+
+    public interface IRectangle
+    {
+        int Width { get; }
+        int Height { get; }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Area(this IRectangle rc)
+        {
+            return rc.Width * rc.Height;
+        }
+    }
+
+    public class SquareToRectangleAdapter : IRectangle
+    {
+        public SquareToRectangleAdapter(Square square)
+        {
+            Width = square.Side;
+            Height = square.Side;
+        }
+
+        public int Width { get; }
+
+        public int Height { get; }
+    }
+}
