0d0555e8fae7f6d44c8cd3c7eb38e3c0c0181fa6
Hiroshi Takeda <takejaponpon+git@gmail.com>
2022-03-20 01:04:32 +0900

    add interpreter design pattern

---
+++ Coding/Exercise/interpreter-design.test.cs
@@ -0,0 +1,34 @@
+﻿using Coding.Exercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Coding.Exercise;
+
+public class UnitTestInterpreter
+{
+    [Fact]
+    public void Test1()
+    {
+        var interpreter = new ExpressionProcessor();
+        Assert.Equal(6, interpreter.Calculate("1+2+3"));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var interpreter = new ExpressionProcessor();
+        Assert.Equal(0, interpreter.Calculate("1+2+xy"));
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var interpreter = new ExpressionProcessor();
+        interpreter.Variables['x'] = 3;
+        Assert.Equal(5, interpreter.Calculate("10-2-x"));
+    }
+}
