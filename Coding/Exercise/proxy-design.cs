37d5c55934635e4295ddceea0c12baafa12d9d77
Hiroshi Takeda <takejaponpon+git@gmail.com>
2022-03-19 17:35:16 +0900

    add proxy design

---
+++ Coding/Exercise/proxy-design.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public interface IPerson
+    {
+        int Age { get; set; }
+
+        string Drink();
+        string DrinkAndDrive();
+        string Drive();
+    }
+
+    public class Person : IPerson
+    {
+        public int Age { get; set; }
+
+        public string Drink()
+        {
+            return "drinking";
+        }
+
+        public string Drive()
+        {
+            return "driving";
+        }
+
+        public string DrinkAndDrive()
+        {
+            return "driving while drunk";
+        }
+    }
+
+    public class ResponsiblePerson : IPerson
+    {
+        private readonly Person person;
+
+        public ResponsiblePerson(Person person)
+        {
+            this.person = person;
+        }
+
+        public int Age { 
+            get => person.Age;
+            set => person.Age = value;
+        }
+
+        public string Drink()
+        {
+            if (Age < 18)
+            {
+                return "too young";
+            }
+            return person.Drink();
+        }
+
+        public string DrinkAndDrive()
+        {
+            return "dead";
+        }
+
+        public string Drive()
+        {
+            if (Age < 16)
+            {
+                return "too young";
+            }
+            return person.Drive();
+        }
+    }
+}
---
+++ Coding/Exercise/proxy-design.test.cs
@@ -0,0 +1,52 @@
+﻿using Coding.Exercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Coding.Exercise;
+
+public class UnitTestProxy
+{
+    [Fact]
+    public void Test_Age15()
+    {
+        var person = new Person()
+        {
+            Age = 15,
+        };
+        var proxy = new ResponsiblePerson(person);
+        Assert.Equal("too young", proxy.Drive());
+        Assert.Equal("too young", proxy.Drink());
+        Assert.Equal("dead", proxy.DrinkAndDrive());
+    }
+
+    [Fact]
+    public void Test_Age16()
+    {
+        var person = new Person()
+        {
+            Age = 16,
+        };
+        var proxy = new ResponsiblePerson(person);
+        Assert.Equal("driving", proxy.Drive());
+        Assert.Equal("too young", proxy.Drink());
+        Assert.Equal("dead", proxy.DrinkAndDrive());
+    }
+
+
+    [Fact]
+    public void Test_Age18()
+    {
+        var person = new Person()
+        {
+            Age = 18,
+        };
+        var proxy = new ResponsiblePerson(person);
+        Assert.Equal("driving", proxy.Drive());
+        Assert.Equal("drinking", proxy.Drink());
+        Assert.Equal("dead", proxy.DrinkAndDrive());
+    }
+}
