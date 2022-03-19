6306e0de8c25594e1cab27ec8193bf74525955df
Hiroshi Takeda <takejaponpon+git@gmail.com>
2022-03-19 17:35:07 +0900

    comment out factory design for Person class

--- Coding/Exercise/factory-design.cs
+++ Coding/Exercise/factory-design.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//using System;
+//using System.Collections.Generic;
 
-namespace Coding.Exercise
-{
-    public class Person
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
+//namespace Coding.Exercise
+//{
+//    public class Person
+//    {
+//        public int Id { get; set; }
+//        public string Name { get; set; }
 
-        public Person(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
-    }
+//        public Person(int id, string name)
+//        {
+//            Id = id;
+//            Name = name;
+//        }
+//    }
 
-    public class PersonFactory
-    {
-        private List<WeakReference<Person>> list = new List<WeakReference<Person>>();
-        public Person CreatePerson(string name)
-        {
-            var instance = new Person(list.Count, name);
-            list.Add(new WeakReference<Person>(instance));
-            return instance;
-        }
-    }
-}
+//    public class PersonFactory
+//    {
+//        private List<WeakReference<Person>> list = new List<WeakReference<Person>>();
+//        public Person CreatePerson(string name)
+//        {
+//            var instance = new Person(list.Count, name);
+//            list.Add(new WeakReference<Person>(instance));
+//            return instance;
+//        }
+//    }
+//}
