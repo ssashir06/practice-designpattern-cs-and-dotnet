c132c3480f2aa34e7fff367efa13de896e8b5a77
Hiroshi Takeda <takejaponpon+git@gmail.com>
2022-03-19 23:38:46 +0900

    add chain-of-responsibility design pattern

---
+++ Coding/Exercise/chain-of-responsibility-design.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Coding.Exercise
+{
+    public abstract class Creature
+    {
+        public int Attack { get { return AttackBase + Game.ModifierAttack(this); } }
+        public int Defense { get { return DefenceBase + Game.ModifierDefence(this); } }
+        public int AttackBase { get; set; }
+        public int DefenceBase { get; set; }
+        public Game Game { get; }
+
+        public Creature(Game game)
+        {
+            Game = game;
+        }
+
+        public abstract int ModifierAttack(Creature creature, int v);
+
+        public abstract int ModifierDefence(Creature creature, int v);
+    }
+
+    public class Goblin : Creature
+    {
+        public Goblin(Game game) : base(game)
+        {
+            DefenceBase = AttackBase = 1;
+        }
+
+        public override int ModifierAttack(Creature creature, int v)
+        {
+            return 0;
+        }
+
+        public override int ModifierDefence(Creature creature, int v)
+        {
+            if (creature is Goblin && !creature.Equals(this))
+            {
+                v += 1;
+            }
+            return v;
+        }
+    }
+
+    public class GoblinKing : Goblin
+    {
+        public GoblinKing(Game game) : base(game)
+        {
+            DefenceBase = AttackBase = 3;
+        }
+
+        public override int ModifierAttack(Creature creature, int v)
+        {
+            if (creature is Goblin && !creature.Equals(this))
+            {
+                v += 1;
+            }
+            return v;
+        }
+    }
+
+    public class Game
+    {
+        public IList<Creature> Creatures = new List<Creature>();
+
+        internal int ModifierAttack(Creature creature)
+        {
+            var v = 0;
+            foreach (var c in Creatures)
+            {
+                v = c.ModifierAttack(creature, v);
+            }
+            return v;
+        }
+
+        internal int ModifierDefence(Creature creature)
+        {
+            var v = 0;
+            foreach (var c in Creatures)
+            {
+                v = c.ModifierDefence(creature, v);
+            }
+            return v;
+        }
+    }
+}
---
+++ Coding/Exercise/chain-of-responsibility-design.test.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Xunit;
+
+namespace Coding.Exercise;
+
+public class UnitTestChainOfResponsibility
+{
+    [Fact]
+    public void Test1()
+    {
+        var game = new Game();
+        var goblin = new Goblin(game);
+        game.Creatures.Add(goblin);
+        Assert.Equal(1, goblin.Attack);
+        Assert.Equal(1, goblin.Defense);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var game = new Game();
+        var goblin1 = new Goblin(game);
+        var goblin2 = new Goblin(game);
+        game.Creatures.Add(goblin1);
+        game.Creatures.Add(goblin2);
+        Assert.Equal(1, goblin1.Attack);
+        Assert.Equal(2, goblin1.Defense);
+        Assert.Equal(1, goblin2.Attack);
+        Assert.Equal(2, goblin2.Defense);
+        game.Creatures.Remove(goblin1);
+        Assert.Equal(1, goblin2.Attack);
+        Assert.Equal(1, goblin2.Defense);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var game = new Game();
+        var goblinKing1 = new GoblinKing(game);
+        game.Creatures.Add(goblinKing1);
+        Assert.Equal(3, goblinKing1.Attack);
+        Assert.Equal(3, goblinKing1.Defense);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var game = new Game();
+        var goblin1 = new Goblin(game);
+        var goblin2 = new Goblin(game);
+        var goblin3 = new Goblin(game);
+        game.Creatures.Add(goblin1);
+        game.Creatures.Add(goblin2);
+        game.Creatures.Add(goblin3);
+        Assert.Equal(1, goblin1.Attack);
+        Assert.Equal(3, goblin1.Defense);
+        Assert.Equal(1, goblin2.Attack);
+        Assert.Equal(3, goblin2.Defense);
+        Assert.Equal(1, goblin3.Attack);
+        Assert.Equal(3, goblin3.Defense);
+        var goblinKing1 = new GoblinKing(game);
+        game.Creatures.Add(goblinKing1);
+        Assert.Equal(2, goblin1.Attack);
+        Assert.Equal(4, goblin1.Defense);
+        Assert.Equal(2, goblin2.Attack);
+        Assert.Equal(4, goblin2.Defense);
+        Assert.Equal(2, goblin3.Attack);
+        Assert.Equal(4, goblin3.Defense);
+        Assert.Equal(3, goblinKing1.Attack);
+        Assert.Equal(6, goblinKing1.Defense);
+
+    }
+
+}
