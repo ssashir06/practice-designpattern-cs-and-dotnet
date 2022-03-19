bdc696fae4b532de9753d690f9c7c60b2e24ccc1
Hiroshi Takeda <takejaponpon+git@gmail.com>
2022-03-19 11:47:59 +0900

    comment out adapter design for bridge design

--- Coding/Exercise/adapter-design.test.cs
+++ Coding/Exercise/adapter-design.test.cs
@@ -1,21 +1,21 @@
-using System;
-using Xunit;
+// using System;
+// using Xunit;
 
-namespace Coding.Exercise;
+// namespace Coding.Exercise;
 
-public class UnitTestAdapter
-{
-    [Fact]
-    public void Test1()
-    {
-        var square = new Square()
-        {
-            Side = 5,
-        };
-        var rectangle = new SquareToRectangleAdapter(square);
+// public class UnitTestAdapter
+// {
+//     [Fact]
+//     public void Test1()
+//     {
+//         var square = new Square()
+//         {
+//             Side = 5,
+//         };
+//         var rectangle = new SquareToRectangleAdapter(square);
 
-        Assert.Equal(5, rectangle.Width);
-        Assert.Equal(5, rectangle.Height);
-    }
+//         Assert.Equal(5, rectangle.Width);
+//         Assert.Equal(5, rectangle.Height);
+//     }
 
-}
+// }
