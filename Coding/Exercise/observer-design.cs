4a4cf3f95b0d87a660425ea70a2c2ce533e109fe
Hiroshi Takeda <takejaponpon+git@gmail.com>
2022-03-21 16:48:48 +0900

    add observer design pattern

---
+++ Coding/Exercise/observer-design.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Coding.Exercise
+{
+    public class Game
+    {
+        public event EventHandler<RatCountEventArgs> RatsUpdated;
+        public void UpdateCountOfRats(int count)
+        {
+            RatsUpdated?.Invoke(this, new RatCountEventArgs() { Count = count });
+        }
+    }
+
+    public class RatCountEventArgs
+    {
+        public int Count { get; set; }
+    }
+
+    public class Rat : IDisposable
+    {
+        public int Attack = 1;
+        public static Dictionary<Game, int> AttackMap = new Dictionary<Game, int>();
+        private readonly Game game;
+
+        public Rat(Game game)
+        {
+            this.game = game;
+            if (!AttackMap.ContainsKey(game))
+            {
+                AttackMap[game] = 0;
+            }
+            AttackMap[game]++;
+
+            game.RatsUpdated += Game_RatsUpdated;
+
+            game.UpdateCountOfRats(AttackMap[game]);
+        }
+
+        private void Game_RatsUpdated(object sender, RatCountEventArgs e)
+        {
+            this.Attack = e.Count;
+        }
+
+        public void Dispose()
+        {
+            this.game.RatsUpdated -= Game_RatsUpdated;
+            AttackMap[this.game]--;
+            this.game.UpdateCountOfRats(AttackMap[game]);
+        }
+    }
+}
+
---
+++ Coding/Exercise/observer-design.test.cs
@@ -0,0 +1,29 @@
+﻿using Coding.Exercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Coding.Exercise;
+
+public class UnitTestObserver
+{
+    [Fact]
+    public void Test1()
+    {
+        var game = new Game();
+        using (var rat1 = new Rat(game))
+        {
+
+            Assert.Equal(1, rat1.Attack);
+            using (var rat2 = new Rat(game))
+            {
+                Assert.Equal(2, rat1.Attack);
+                Assert.Equal(2, rat2.Attack);
+            }
+            Assert.Equal(1, rat1.Attack);
+        }
+    }
+}
