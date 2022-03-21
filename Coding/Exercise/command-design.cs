7c886ecb150be3ac7eb1c9ec02e75ed6fa729a67
Hiroshi Takeda <takejaponpon+git@gmail.com>
2022-03-21 14:06:19 +0900

    comment out command design pattern for Account class

--- Coding/Exercise/command-design.cs
+++ Coding/Exercise/command-design.cs
@@ -1,48 +1,48 @@
-﻿using System;
+﻿// using System;
 
-namespace Coding.Exercise
-{
-    public class Command
-    {
-        public enum Action
-        {
-            Deposit,
-            Withdraw
-        }
+// namespace Coding.Exercise
+// {
+//     public class Command
+//     {
+//         public enum Action
+//         {
+//             Deposit,
+//             Withdraw
+//         }
 
-        public Action TheAction;
-        public int Amount;
-        public bool Success;
-    }
+//         public Action TheAction;
+//         public int Amount;
+//         public bool Success;
+//     }
 
-    public class Account
-    {
-        public int Balance { get; set; }
+//     public class Account
+//     {
+//         public int Balance { get; set; }
 
-        public void Process(Command c)
-        {
-            switch (c.TheAction)
-            {
-                case Command.Action.Deposit:
-                    Balance += c.Amount;
-                    c.Success = true;
-                    Console.WriteLine($"Proceeded: Add {c.Amount}, Total {Balance}");
-                    break;
-                case Command.Action.Withdraw:
-                    if (Balance >= c.Amount)
-                    {
-                        Balance -= c.Amount;
-                        c.Success = true;
-                        Console.WriteLine($"Proceeded: Minus {c.Amount}, Total {Balance}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Cannot proceeded: Minus {c.Amount}, Total {Balance}");
-                    }
-                    break;
-                default:
-                    throw new ArgumentException();
-            }
-        }
-    }
-}
+//         public void Process(Command c)
+//         {
+//             switch (c.TheAction)
+//             {
+//                 case Command.Action.Deposit:
+//                     Balance += c.Amount;
+//                     c.Success = true;
+//                     Console.WriteLine($"Proceeded: Add {c.Amount}, Total {Balance}");
+//                     break;
+//                 case Command.Action.Withdraw:
+//                     if (Balance >= c.Amount)
+//                     {
+//                         Balance -= c.Amount;
+//                         c.Success = true;
+//                         Console.WriteLine($"Proceeded: Minus {c.Amount}, Total {Balance}");
+//                     }
+//                     else
+//                     {
+//                         Console.WriteLine($"Cannot proceeded: Minus {c.Amount}, Total {Balance}");
+//                     }
+//                     break;
+//                 default:
+//                     throw new ArgumentException();
+//             }
+//         }
+//     }
+// }
--- Coding/Exercise/command-design.test.cs
+++ Coding/Exercise/command-design.test.cs
@@ -1,56 +1,56 @@
-﻿using Coding.Exercise;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿// using Coding.Exercise;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+// using Xunit;
 
-namespace Coding.Exercise;
+// namespace Coding.Exercise;
 
-public class UnitTestCommand
-{
-    [Fact]
-    public void Test1()
-    {
-        var account = new Account();
-        var commands = new[]
-        {
-            new Command() { TheAction = Command.Action.Deposit, Amount = 100 },
-            new Command() { TheAction = Command.Action.Withdraw, Amount = 50 },
-            new Command() { TheAction = Command.Action.Withdraw, Amount = 50 },
-            new Command() { TheAction = Command.Action.Withdraw, Amount = 50 },
-        };
-        foreach (var command in commands)
-        {
-            account.Process(command);
-            if (!command.Success)
-            {
-                break;
-            }
-        }
-        Assert.Equal(0, account.Balance);
-    }
+// public class UnitTestCommand
+// {
+//     [Fact]
+//     public void Test1()
+//     {
+//         var account = new Account();
+//         var commands = new[]
+//         {
+//             new Command() { TheAction = Command.Action.Deposit, Amount = 100 },
+//             new Command() { TheAction = Command.Action.Withdraw, Amount = 50 },
+//             new Command() { TheAction = Command.Action.Withdraw, Amount = 50 },
+//             new Command() { TheAction = Command.Action.Withdraw, Amount = 50 },
+//         };
+//         foreach (var command in commands)
+//         {
+//             account.Process(command);
+//             if (!command.Success)
+//             {
+//                 break;
+//             }
+//         }
+//         Assert.Equal(0, account.Balance);
+//     }
 
-    [Fact]
-    public void Test2()
-    {
-        var account = new Account();
-        var commands = new[]
-        {
-            new Command() { TheAction = Command.Action.Deposit, Amount = 100 },
-            new Command() { TheAction = Command.Action.Withdraw, Amount = 40 },
-            new Command() { TheAction = Command.Action.Withdraw, Amount = 50 },
-            new Command() { TheAction = Command.Action.Withdraw, Amount = 50 },
-        };
-        foreach (var command in commands)
-        {
-            account.Process(command);
-            if (!command.Success)
-            {
-                break;
-            }
-        }
-        Assert.Equal(10, account.Balance);
-    }
-}
+//     [Fact]
+//     public void Test2()
+//     {
+//         var account = new Account();
+//         var commands = new[]
+//         {
+//             new Command() { TheAction = Command.Action.Deposit, Amount = 100 },
+//             new Command() { TheAction = Command.Action.Withdraw, Amount = 40 },
+//             new Command() { TheAction = Command.Action.Withdraw, Amount = 50 },
+//             new Command() { TheAction = Command.Action.Withdraw, Amount = 50 },
+//         };
+//         foreach (var command in commands)
+//         {
+//             account.Process(command);
+//             if (!command.Success)
+//             {
+//                 break;
+//             }
+//         }
+//         Assert.Equal(10, account.Balance);
+//     }
+// }
