d89a36b7f53de4b2ad5723db0981c76f15e1479f
Hiroshi Takeda <takejaponpon+git@gmail.com>
2022-03-21 22:21:06 +0900

    add template design pattern

---
+++ Coding/Exercise/template-design.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+
+namespace Coding.Exercise
+{
+    public class Creature
+    {
+        public int Attack, Health;
+
+        public Creature(int attack, int health)
+        {
+            Attack = attack;
+            Health = health;
+        }
+    }
+
+    public abstract class CardGame
+    {
+        public Creature[] Creatures;
+
+        public CardGame(Creature[] creatures)
+        {
+            Creatures = creatures;
+        }
+
+        // returns -1 if no clear winner (both alive or both dead)
+        public int Combat(int creature1, int creature2)
+        {
+            StartOfCombat();
+            Creature first = Creatures[creature1];
+            Creature second = Creatures[creature2];
+            Hit(first, second);
+            Hit(second, first);
+            bool firstAlive = first.Health > 0;
+            bool secondAlive = second.Health > 0;
+            EndOfCombat();
+            if (firstAlive == secondAlive) return -1;
+            return firstAlive ? creature1 : creature2;
+        }
+
+        protected abstract void EndOfCombat();
+        protected abstract void StartOfCombat();
+
+        protected void Hit(Creature attacker, Creature other)
+        {
+            other.Health -= attacker.Attack;
+        }
+    }
+
+    public class TemporaryCardDamageGame : CardGame
+    {
+        private Creature[] CreaturesBackup = new Creature[0];
+
+        public TemporaryCardDamageGame(Creature[] creatures) : base(creatures)
+        {
+        }
+
+        protected override void StartOfCombat()
+        {
+            CreaturesBackup = Creatures
+                .Select(c => new Creature(c.Attack, c.Health))
+                .ToArray();
+        }
+
+        protected override void EndOfCombat()
+        {
+            Creatures = CreaturesBackup;
+        }
+    }
+
+    public class PermanentCardDamage : CardGame
+    {
+        public PermanentCardDamage(Creature[] creatures) : base(creatures)
+        {
+        }
+
+        protected override void EndOfCombat()
+        {
+        }
+
+        protected override void StartOfCombat()
+        {
+        }
+    }
+}
---
+++ Coding/Exercise/template-design.test.cs
@@ -0,0 +1,34 @@
+﻿using Coding.Exercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Coding.Exercise;
+
+public class UnitTestTemplate
+{
+    [Fact]
+    public void Test_Temporary_1()
+    {
+        var game = new TemporaryCardDamageGame(
+            new[] { new Creature(1, 2), new Creature(1, 3)}
+            );
+        Assert.Equal(-1, game.Combat(0, 1));
+        Assert.Equal(-1, game.Combat(0, 1));
+        Assert.Equal(-1, game.Combat(0, 1));
+    }
+
+    [Fact]
+    public void Test_Permanent_1()
+    {
+        var game = new PermanentCardDamage(
+            new[] { new Creature(1, 2), new Creature(1, 3) }
+            );
+        Assert.Equal(-1, game.Combat(0, 1));
+        Assert.Equal(1, game.Combat(0, 1));
+    }
+}
