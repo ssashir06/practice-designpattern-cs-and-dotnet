9a4b20cc6d6cee650e66b6d2c1995f3001257956
Hiroshi Takeda <takejaponpon+git@gmail.com>
2022-03-21 17:26:20 +0900

    Add state design pattern

---
+++ Coding/Exercise/state-design.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Coding.Exercise
+{
+    public class CombinationLock
+    {
+        private readonly int[] correctCombination;
+
+        public CombinationLock(int[] combination)
+        {
+            correctCombination = combination;
+        }
+
+        public string Status = "LOCKED";
+
+        public void EnterDigit(int digit)
+        {
+            if (!new Regex("^\\d+$").IsMatch(Status))
+            {
+                Status = "";
+            }
+            Status += $"{digit}";
+            Console.WriteLine(Status);
+            if (string.Join("", correctCombination.Select(v => v.ToString())) == Status)
+            {
+                Status = "OPEN";
+            }
+            else if (Status.Length >= correctCombination.Length)
+            {
+                Status = "ERROR";
+            }
+        }
+    }
+}
---
+++ Coding/Exercise/state-design.test.cs
@@ -0,0 +1,63 @@
+﻿using Coding.Exercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Coding.Exercise;
+
+public class UnitTestState
+{
+    [Fact]
+    public void Test_Success()
+    {
+        var cl = new CombinationLock(new[] { 1, 2, 3, 4, 5 });
+        Assert.Equal("LOCKED", cl.Status);
+        cl.EnterDigit(1);
+        Assert.Equal("1", cl.Status);
+        cl.EnterDigit(2);
+        Assert.Equal("12", cl.Status);
+        cl.EnterDigit(3);
+        Assert.Equal("123", cl.Status);
+        cl.EnterDigit(4);
+        Assert.Equal("1234", cl.Status);
+        cl.EnterDigit(5);
+        Assert.Equal("OPEN", cl.Status);
+    }
+
+    [Fact]
+    public void Test_Failed_1()
+    {
+        var cl = new CombinationLock(new[] { 1, 2, 3, 4, 5 });
+        Assert.Equal("LOCKED", cl.Status);
+        cl.EnterDigit(1);
+        Assert.Equal("1", cl.Status);
+        cl.EnterDigit(2);
+        Assert.Equal("12", cl.Status);
+        cl.EnterDigit(3);
+        Assert.Equal("123", cl.Status);
+        cl.EnterDigit(4);
+        Assert.Equal("1234", cl.Status);
+        cl.EnterDigit(9);
+        Assert.Equal("ERROR", cl.Status);
+    }
+
+    [Fact]
+    public void Test_Failed_2()
+    {
+        var cl = new CombinationLock(new[] { 1, 2, 3, 4, 5 });
+        Assert.Equal("LOCKED", cl.Status);
+        cl.EnterDigit(1);
+        Assert.Equal("1", cl.Status);
+        cl.EnterDigit(2);
+        Assert.Equal("12", cl.Status);
+        cl.EnterDigit(3);
+        Assert.Equal("123", cl.Status);
+        cl.EnterDigit(9);
+        Assert.Equal("1239", cl.Status);
+        cl.EnterDigit(9);
+        Assert.Equal("ERROR", cl.Status);
+    }
+}
