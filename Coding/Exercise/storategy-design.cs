6be715ade6ad5a2646ebd1a3f1377d07c70d27e6
Hiroshi Takeda <takejaponpon+git@gmail.com>
2022-03-21 21:39:33 +0900

    add storategy design pattern

---
+++ Coding/Exercise/storategy-design.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Numerics;
+
+namespace Coding.Exercise
+{
+    public interface IDiscriminantStrategy
+    {
+        double CalculateDiscriminant(double a, double b, double c);
+    }
+
+    public class OrdinaryDiscriminantStrategy : IDiscriminantStrategy
+    {
+        public double CalculateDiscriminant(double a, double b, double c)
+        {
+            return b * b - 4 * a * c;
+        }
+    }
+
+    public class RealDiscriminantStrategy : IDiscriminantStrategy
+    {
+        // todo (return NaN on negative discriminant!)
+        public double CalculateDiscriminant(double a, double b, double c)
+        {
+            var val = b * b - 4 * a * c;
+            if (val < 0)
+            {
+                return double.NaN;
+            }
+            else
+            {
+                return val;
+            }
+        }
+    }
+
+    public class QuadraticEquationSolver
+    {
+        private readonly IDiscriminantStrategy strategy;
+
+        public QuadraticEquationSolver(IDiscriminantStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public Tuple<Complex, Complex> Solve(double a, double b, double c)
+        {
+            var d = this.strategy.CalculateDiscriminant(a, b, c);
+            return Tuple.Create(
+                (-b + Complex.Sqrt(d)) / (2 * a), 
+                (-b - Complex.Sqrt(d)) / (2 * a));
+        }
+    }
+}
---
+++ Coding/Exercise/storategy-design.test.cs
@@ -0,0 +1,37 @@
+﻿using Coding.Exercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Coding.Exercise;
+
+public class UnitTestStorategy
+{
+    [Fact]
+    public void Test_Ordinary()
+    {
+        var calc = new QuadraticEquationSolver(new OrdinaryDiscriminantStrategy());
+        Assert.Equal(Tuple.Create(new Complex(3, 0), new Complex(-1, 0)),
+            calc.Solve(1, -2, -3));
+        Assert.Equal(Tuple.Create(new Complex(1, 0), new Complex(-1.5, 0)),
+            calc.Solve(2, 1, -3));
+        Assert.Equal(Tuple.Create(new Complex(1, Math.Sqrt(2)), new Complex(1, -Math.Sqrt(2))),
+            calc.Solve(1, -2, 3));
+    }
+
+    [Fact]
+    public void Test_Real()
+    {
+        var calc = new QuadraticEquationSolver(new RealDiscriminantStrategy());
+        Assert.Equal(Tuple.Create(new Complex(3, 0), new Complex(-1, 0)),
+            calc.Solve(1, -2, -3));
+        Assert.Equal(Tuple.Create(new Complex(1, 0), new Complex(-1.5, 0)),
+            calc.Solve(2, 1, -3));
+        Assert.Equal(Tuple.Create(Complex.NaN, Complex.NaN),
+            calc.Solve(1, -2, 3));
+    }
+}
